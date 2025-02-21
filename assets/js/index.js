3c9b34ac16f2027e814820f5d454bf45481986d9
phmagalhaes <pedrohenrique.msantos.2001@gmail.com>
2025-02-21 10:47:43 -0300

    commit 21/02

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,15 @@
 const banner = document.getElementById("banner");
 function carrossel(){
     let i = 1;
+    banner.style.backgroundImage = `url('../assets/images/index/${i}.jpg')`
     setInterval(() => {
+        i++
         if(i < 8){
             banner.style.backgroundImage = `url('../assets/images/index/${i}.jpg')`
-            i++
         } else{
             i = 1
             banner.style.backgroundImage = `url('../assets/images/index/${i}.jpg')`
         }
-    }, 4000);
+    }, 3000);
 }
 carrossel()
